62c344d3f157bb5d699fab9182fda64056b629d4
WindyFeg <phongspeed7@yahoo.com.vn>
2023-04-26 10:46:08 +0700

    Init folder and file

---
+++ index.php
@@ -0,0 +1,22 @@
+<html lang="en">
+
+<body>
+    <?php
+    include_once 'template/header.php';
+    ?>
+    <div class="body">
+        <?php
+        if (isset($_GET['page'])) {
+            $page = $_GET['page'];
+            include "$page.php";
+        } else {
+            include 'pages/home.php';
+        }
+        ?>
+    </div>
+    <?php
+    include_once 'template/footer.php';
+    ?>
+</body>
+
+</html>
---
+++ template/header.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/index.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>My website</title>
+</head>
+
+<div> THIS IS HEADER</div>
