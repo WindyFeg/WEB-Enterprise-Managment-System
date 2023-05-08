3c220b3b14beef51a46e0e4494eb555940e6456c
DatDangTien <116250191+DatDangTien@users.noreply.github.com>
2023-05-08 18:37:46 +0700

    Fix bug on drop table and foreign key assign
    Drop the reference table (with foreign key) first before drop the original table. Unexplanable bug when assign foreign key with not null attribute in result.task_id.

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,16 @@
--- USE `ManageSys`
+USE `ManageSys` ;
 
+DROP TABLE IF EXISTS `assign_task` ;
+DROP TABLE IF EXISTS `result` ;
+DROP TABLE IF EXISTS `task` ;
+DROP TABLE IF EXISTS `employee` ;
+DROP TABLE IF EXISTS `dep_head` ;
+DROP TABLE IF EXISTS `director` ;
+DROP TABLE IF EXISTS `department` ;
 DROP TABLE IF EXISTS `user` ;
+
 CREATE TABLE user (
-    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY ,
+    id INT(11) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY ,
     username VARCHAR(50) NOT NULL UNIQUE,
     `password` VARCHAR(50) NOT NULL,
     lv INT(6) UNSIGNED NOT NULL,
@@ -18,69 +26,63 @@ CREATE TABLE user (
     gender ENUM('male', 'female')
 ) ;
 
-DROP TABLE IF EXISTS `director` ;
+CREATE TABLE department (
+    dept_id INT(11) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY ,
+    dept_name VARCHAR(50) NOT NULL,
+    emp_count INT(6)
+) ;
+
 CREATE TABLE director (
-    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY ,
+    id INT(11) UNSIGNED NOT NULL PRIMARY KEY ,
     title VARCHAR(50) ,
     FOREIGN KEY (id) REFERENCES user(id) ON DELETE CASCADE
 ) ;
 
-DROP TABLE IF EXISTS `dep_head` ;
 CREATE TABLE dep_head (
-    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY ,
-    dept INT(11) ,
-    dir INT(11),
+    id INT(11) UNSIGNED NOT NULL PRIMARY KEY ,
+    dept INT(11) UNSIGNED ,
+    dir INT(11) UNSIGNED ,
     FOREIGN KEY (id) REFERENCES user(id) ON DELETE CASCADE ,
     FOREIGN KEY (dept) REFERENCES department(dept_id) ON DELETE SET NULL ,
     FOREIGN KEY (dir) REFERENCES director(id) ON DELETE SET NULL
 ) ;
 
-DROP TABLE IF EXISTS `employee` ;
 CREATE TABLE employee (
-    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY ,
-    dept INT(11) ,
+    id INT(11) UNSIGNED NOT NULL PRIMARY KEY ,
+    dept INT(11) UNSIGNED ,
     start_dt datetime DEFAULT CURRENT_TIMESTAMP ,
-    dir INT(11) ,
+    dir INT(11) UNSIGNED ,
     FOREIGN KEY (id) REFERENCES user(id) ON DELETE CASCADE ,
     FOREIGN KEY (dept) REFERENCES department(dept_id) ON DELETE SET NULL ,
     FOREIGN KEY (dir) REFERENCES director(id) ON DELETE SET NULL    
 ) ;
 
-DROP TABLE IF EXISTS `department` ;
-CREATE TABLE department (
-    dept_id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY ,
-    dept_name VARCHAR(50) NOT NULL,
-    emp_count INT(6)
-) ;
-
-DROP TABLE IF EXISTS `task` ;
 CREATE TABLE task (
-    task_id INT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY ,
+    task_id INT(20) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY ,
     descrip LONGBLOB ,
     deadline datetime ,
     comment TEXT ,
-    boss_id INT(11) ,
-    FOREIGN KEY (boss_id) REFERENCES user(id) ON DELETE SET NULL ,
+    boss_id INT(11) UNSIGNED ,
+    FOREIGN KEY (boss_id) REFERENCES user(id) ON DELETE SET NULL
 ) ;
 
-DROP TABLE IF EXISTS `result` ;
 CREATE TABLE result (
-    result_id INT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY ,
-    task_id INT(11) NOT NULL ,
+    result_id INT(20) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY ,
+--     NOT NULL in task_id will raise error in create table
+    task_id INT(20) UNSIGNED , 
     -- file??
     res LONGBLOB NOT NULL ,
     comment TEXT ,
-    boss_id INT(11),
-    emp_id INT(11) NOT NULL ,
-    FOREIGN KEY (task_id) REFERENCES task(task_id) ON DELETE SET NULL ,
+    boss_id INT(11) UNSIGNED ,
+    emp_id INT(11) UNSIGNED NOT NULL ,
+    FOREIGN KEY (task_id) REFERENCES task(`task_id`) ON DELETE SET NULL ,
     FOREIGN KEY (boss_id) REFERENCES user(id) ON DELETE SET NULL ,
     FOREIGN KEY (emp_id) REFERENCES user(id) ON DELETE CASCADE
 ) ;
 
-DROP TABLE IF EXISTS `assign_task` ;
 CREATE TABLE assign_task (
-    task_id INT(20) NOT NULL ,
-    emp_id INT(11) NOT NULL ,
+    task_id INT(20) UNSIGNED NOT NULL ,
+    emp_id INT(11) UNSIGNED NOT NULL ,
     PRIMARY KEY (task_id, emp_id) ,
     FOREIGN KEY (task_id) REFERENCES task(task_id) ON DELETE CASCADE ,
     FOREIGN KEY (emp_id) REFERENCES user(id) ON DELETE CASCADE
